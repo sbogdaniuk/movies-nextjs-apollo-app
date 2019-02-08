871feb657ab01a22903a98495ef05b3f1ca1abe8
sergeibogdaniuk <sergei.bogdaniuk@youshido.com>
2019-02-08 12:18:01 +0200

    test now integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,20 +7,21 @@ import { routes } from '../constants'
 const shows = new Array(10).fill(0).map((_, i) => i + 1)
 
 const IndexPage: React.FunctionComponent = () => {
-    return (
-        <Layout title="Home | Next.js + TypeScript Example">
-            <h1>hello Next.js 👋</h1>
-            <ul>
-                {shows.map(id => (
-                    <li key={id}>
-                        <Link route={routes.show} params={{ id }}>
-                            <a>{`Show ${id}`}</a>
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </Layout>
-    )
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+      <h1>hello Next.js 👋</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet dignissimos, dolore ea enim eos esse eum ex laudantium odit optio, placeat porro ratione reiciendis unde vel veniam veritatis voluptatibus!</p>
+      <ul>
+        {shows.map(id => (
+          <li key={id}>
+            <Link route={routes.show} params={{ id }}>
+              <a>{`Show ${id}`}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  )
 }
 
 export default IndexPage
--- pages/show.tsx
+++ pages/show.tsx
@@ -1,45 +1,45 @@
 import * as React from 'react'
-import { NextFunctionComponent } from 'next'
+import { NextFunctionComponent, NextContext } from 'next'
 
 import { Layout } from '../components'
 import { ShowComponent } from '../generated/apolloComponents'
 
-interface Props {
-    id: string
+type Props = {
+  id: string
 }
 
-const Show: NextFunctionComponent = ({ id }: Props) => {
-    return (
-        <ShowComponent variables={{ id }}>
-            {({ error, data }) => {
-                if (error) {
-                    return (
-                        <div>
-                            Error
-                        </div>
-                    )
-                }
-                if (data && data.show) {
-                    const { show } = data
-                    return (
-                        <Layout title={show.name}>
-                            <h1>{show.name}</h1>
-                            <p>Genres: {show.genres.join(', ')}</p>
-                            <img src={show.image.medium as string} alt={`Preview for ${show.name}`} />
-                            <p>Language: {show.language}</p>
-                            <div dangerouslySetInnerHTML={{ __html: show.summary as string }}/>
-                            <p>Type: {show.type}</p>
-                        </Layout>
-                    )
-                }
-                return null
-            }}
-        </ShowComponent>
-    )
+const Show: NextFunctionComponent<Props> = ({ id }) => {
+  return (
+    <ShowComponent variables={{ id }}>
+      {({ error, data }) => {
+        if (error) {
+          return (
+            <div>
+              Error
+            </div>
+          )
+        }
+        if (data && data.show) {
+          const { show } = data
+          return (
+            <Layout title={show.name}>
+              <h1>{show.name}</h1>
+              <p>Genres: {show.genres.join(', ')}</p>
+              <img src={show.image.medium as string} alt={`Preview for ${show.name}`} />
+              <p>Language: {show.language}</p>
+              <div dangerouslySetInnerHTML={{ __html: show.summary as string }} />
+              <p>Type: {show.type}</p>
+            </Layout>
+          )
+        }
+        return null
+      }}
+    </ShowComponent>
+  )
 }
 
-Show.getInitialProps = ({ query }) => {
-    return { id: query.id }
+Show.getInitialProps = async ({ query }: NextContext): Promise<Props> => {
+  return { id: query.id as string }
 }
 
 export default Show
